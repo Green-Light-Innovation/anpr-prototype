0fac53ba1091aa620f83d6bfe4bbb95b1fc39ff8
J4CKINS <jackatkins3@gmail.com>
2022-04-25 15:47:20 +0100

    Added method to create object from api data

--- database/objects/car.py
+++ database/objects/car.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from database.engine import DatabaseEngine
 from database.objects.carlocation import CarLocation
 
@@ -16,19 +18,42 @@ class Car:
         return Car(*data)
 
     @staticmethod
+    def load_from_dvla_data(data:dict, locationID:str) -> object:
+        """ Create a car object using DVLA API data """
+
+        return Car(
+            None, # ID
+            data["registrationNumber"], # number plate
+            None, # recorded datetime
+            data["make"], # car make
+            data["yearOfManufacture"], # manufacture year
+            data["co2Emissions"], # emission levels
+            data["fuelType"], # fuel type
+            locationID # location ID
+        )
+
+    @staticmethod
     def load_all() -> list:pass # TODO Implement this
 
-    def __init__(self, ID:str, plate:str, recorded_datetime:int, make:str, manufacture_year:int, emissions:float, fuel_type:str, mot_date:int, car_location:str) -> object:
+    @classmethod
+    def __get_unix_time(cls) -> int:
+        """ Get current datetime in unix time format"""
+        return round(datetime.timestamp(datetime.now()))
+
+    def __init__(self, ID:str, plate:str, recorded_datetime:int, make:str, manufacture_year:int, emissions:float, fuel_type:str, car_location:str) -> object:
         self.__ID = ID
         if not ID: self.__ID = DatabaseEngine.gen_id() # If no ID is specified, gen a new one
 
         self.__plate = plate
+        
+        # If datetime is specified as None, get current datetime in unix format
         self.__recorded_datetime = recorded_datetime
+        if not recorded_datetime: self.__recorded_datetime = Car.__get_unix_time()
+        
         self.__make = make
         self.__manufacture_year = manufacture_year
         self.__emissions = emissions
         self.__fuel_type = fuel_type
-        self.__mot_date = mot_date
         self.__car_location = CarLocation.load_from_database(car_location) # TODO CHANGE TO CAR LOCATION OBJECT
 
     # Getters
@@ -39,7 +64,6 @@ class Car:
     def get_manufacture_year(self) -> int: return self.__manufacture_year
     def get_emissions(self) -> float: return self.__emissions
     def get_fuel_type(self) -> str: return self.__fuel_type
-    def get_mot_date(self) -> str: return self.__mot_date
     def get_car_location(self) -> CarLocation: return self.__car_location
     
     def save_to_database(self) -> None:
@@ -48,7 +72,7 @@ class Car:
         # SQL query to insert new record into Cars table
         query = """
         INSERT INTO Cars (ID, plate, recorded_datetime, make, manufacture_year, emissions, fuel_type, mot_date, car_location)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?);
         """
 
         DatabaseEngine.connect() # Connect to the database
@@ -62,7 +86,6 @@ class Car:
             self.__manufacture_year,
             self.__emissions,
             self.__fuel_type,
-            self.__mot_date,
             self.__car_location.get_ID()
         ))
 
